e3f59dbb15c058965f22c77426fb2253edbd014e
Obligedwalnut <benerogic@gmail.com>
2023-07-19 13:34:25 +0900

    docs : app.use 순서 수정

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ dotenv.config();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use('/api', routes);
+
 app.use(
   cors({
     origin: ['https://escapediary-fe-delta.vercel.app'],
@@ -19,6 +19,7 @@ app.use(
     credentials: true,
   })
 );
+app.use('/api', routes);
 
 app.listen(process.env.PORT || 3000, (req, res) => {
   console.log(`${process.env.PORT || 3000} 포트에 접속 되었습니다.`);
