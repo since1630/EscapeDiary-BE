e5baaccb778a67742cffd5c99f089b92147a7768
Obligedwalnut <benerogic@gmail.com>
2023-07-19 11:23:08 +0900

    feat : index에서 cors 미들웨어 사용

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const posts = require('./posts');
 const users = require('./users')
+const cors = require('cors')
 const router = express.Router();
 
+
+router.use(cors())
 
 router.use('/', users);
 router.use('/posts', posts);
