d0f4cad7cc04c6f2d26a9d1c74057edae8315b63
Shin Sungyun <sinsungyun1630@gmail.com>
2023-07-19 16:40:36 +0900

    docs : auth미들웨어어 try 수정

--- middlewares/auth_middleware.js
+++ middlewares/auth_middleware.js
@@ -1,32 +1,32 @@
-const jwt = require("jsonwebtoken");
-const {Users} = require("../models");
-
+const jwt = require('jsonwebtoken');
+const { Users } = require('../models');
 
 // 사용자 인증 미들웨어
 const verifyToken = async (req, res, next) => {
   // console.log(req.cookies)
-  const { Authorization } = req.cookies;
-  console.log(Authorization)
-  const [authType, authToken] = (Authorization ?? "").split(" ");
-  // console.log(authType)
-  console.log(authToken)
-    if (!authToken || authType !== "Bearer") {
-    res.status(401).send({
-      errorMessage: `${req.cookies}` ,//! 무조건 수정해야함
-    });
-    return;
-  } 
 
   try {
+    const { Authorization } = req.cookies;
+    console.log(Authorization);
+    const [authType, authToken] = (Authorization ?? '').split(' ');
+    // console.log(authType)
+    console.log(authToken);
+    if (!authToken || authType !== 'Bearer') {
+      res.status(401).send({
+        errorMessage: `${req.cookies}`, //! 무조건 수정해야함
+      });
+      return;
+    }
+
     const { id } = jwt.verify(authToken, 'my-secret-key');
-    const user = await Users.findOne({where:{id}});
+    const user = await Users.findOne({ where: { id } });
     res.locals.user = user;
-    
+
     next();
   } catch (err) {
     console.error(err);
     res.status(401).send({
-      errorMessage: "22222.", //! 무조건 수정해야함
+      errorMessage: '22222.', //! 무조건 수정해야함
     });
   }
 };
--- routes/users.js
+++ routes/users.js
@@ -1,114 +1,113 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
-const { Users } = require("../models");
-const jwt = require("jsonwebtoken");
-const verifyToken = require("../middlewares/auth_middleware");
+const { Users } = require('../models');
+const jwt = require('jsonwebtoken');
+const verifyToken = require('../middlewares/auth_middleware');
 
 // 회원가입
-router.post("/signup", async (req, res) => {
+router.post('/signup', async (req, res) => {
   const { id, password, confirm } = req.body;
   try {
     // 값이 다 있는지 확인부터
     if (!id || !password || !confirm) {
       return res
         .status(412)
-        .json({ message: "닉네임과 비밀번호를 모두 입력해주세요." });
+        .json({ message: '닉네임과 비밀번호를 모두 입력해주세요.' });
     }
 
     // id 길이 확인
     if (id.length < 5 || id.length > 15) {
-      return res.status(412).json({ message: "id는 5에서 15글자 입니다." });
+      return res.status(412).json({ message: 'id는 5에서 15글자 입니다.' });
     }
 
     // 정규식 검사(알파벳과 숫자)
     if (!/^[a-z0-9]+$/.test(id)) {
       return res.status(412).json({
-        message: "id는 알파벳 소문자와 숫자로만 구성되어야 합니다.",
+        message: 'id는 알파벳 소문자와 숫자로만 구성되어야 합니다.',
       });
     }
 
     // password 길이 확인
     if (password.length < 6 || password.length > 12) {
       return res.status(412).json({
-        message: "비밀번호는 6자 이상 12자 이하 입니다.",
+        message: '비밀번호는 6자 이상 12자 이하 입니다.',
       });
     }
 
     // 정규식 검사(알파벳과 숫자)
     if (!/^[a-z0-9]+$/.test(password)) {
       return res.status(412).json({
-        message: "비밀번호는 알파벳 소문자와 숫자로만 구성되어야 합니다.",
+        message: '비밀번호는 알파벳 소문자와 숫자로만 구성되어야 합니다.',
       });
     }
 
     // 비밀번호=confirm
     if (password !== confirm) {
       return res.status(412).json({
-        message: "비밀번호가 일치하지 않습니다.",
+        message: '비밀번호가 일치하지 않습니다.',
       });
     }
 
     // id 유니크
-    const existUser = await Users.findOne({where:{ id }});
-    console.log(existUser)
+    const existUser = await Users.findOne({ where: { id } });
+    console.log(existUser);
     if (existUser) {
-      return res.status(412).json({ message: "중복된 id 입니다." });
+      return res.status(412).json({ message: '중복된 id 입니다.' });
     }
 
     // 회원 가입
     await Users.create({ id, password });
-    return res.status(201).json({ message: "회원가입에 성공하였습니다." });
+    return res.status(201).json({ message: '회원가입에 성공하였습니다.' });
   } catch (error) {
     console.log(error);
     return res
       .status(400)
-      .json({ message: "요청한 데이터 형식이 올바르지 않습니다." });
+      .json({ message: '요청한 데이터 형식이 올바르지 않습니다.' });
   }
 });
 
 // 로그인
-router.post("/login", async (req, res) => {
-    try {
+router.post('/login', async (req, res) => {
+  try {
     const { id, password } = req.body;
-    const existUser = await Users.findOne({ where :{id} });
+    const existUser = await Users.findOne({ where: { id } });
     // id 비밀번호 확인
     if (!existUser || password !== existUser.password) {
       return res
         .status(400)
-        .json({ message: " id와 비밀번호를 확인해주세요." });
+        .json({ message: ' id와 비밀번호를 확인해주세요.' });
     }
-    
 
     // 토큰 발급
-    const token = jwt.sign({ id: existUser.id }, "my-secret-key");
+    const token = jwt.sign({ id: existUser.id }, 'my-secret-key');
 
     // cookie로 저장
-    res.cookie("Authorization", `Bearer ${token}`)
+    res.cookie('Authorization', `Bearer ${token}`);
     // res.cookie("Authorization", `Bearer ${token}`, {
     //   secure: false,
     //   httpOnly: ,
     //   sameSite: 'none',
     //   });
-    return res.status(200).json({ message: "로그인에 성공했습니다." });
+    return res.status(200).json({ message: '로그인에 성공했습니다.' });
   } catch (error) {
     console.log(error);
-    return res.status(400).json({ message: "로그인에 실패하였습니다." });
+    return res.status(400).json({ message: '로그인에 실패하였습니다.' });
   }
 });
 
 // user 정보 받는 라우터 하나(verify 쓰고)
-router.get("/user", verifyToken, (req, res) => {
+router.get('/user', verifyToken, async (req, res) => {
   try {
-    return res.status(200).json({message : `${req.cookies}`}) // ! 유저 조회 없이 일단 쿠키만 브라우저에서 보이게
-    const user = res.locals.user;
-    return res.status(200).json({ data: {id : user.id} });
+    // return res.status(200).json({message : `${req.cookies}`}) // ! 유저 조회 없이 일단 쿠키만 브라우저에서 보이게
+    const { id } = res.locals.user;
+    const idNickname = await Users.findOne({ where: { id } });
+    return res.status(200).json({ data: { id: idNickname } });
   } catch (error) {
     console.log(error);
     return res
       .status(400)
-      .json({ message: "유저 정보를 불러오지 못했습니다." });
+      .json({ message: '유저 정보를 불러오지 못했습니다.' });
   }
 });
- 
 
 module.exports = router;
