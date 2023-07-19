d411375dc6d45c510041564c70066f978d36f50e
Obligedwalnut <benerogic@gmail.com>
2023-07-19 11:25:34 +0900

    Feat : cors router index 상세 설정 변경

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,11 @@ const cors = require('cors')
 const router = express.Router();
 
 
-router.use(cors())
+router.use(cors({
+    origin: 'https://escapediary-fe-delta.vercel.app',
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    credentials: true
+  }));
 
 router.use('/', users);
 router.use('/posts', posts);
